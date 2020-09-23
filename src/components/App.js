0a3cda4deaa861c3d9a4e8716c87cb55647f095a
dsch <davidschreiterprogramming@gmail.com>
2020-09-22 22:08:56 -0500

    Add axios, App to class based

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-	return (
-		<div className='ui container' style={{ marginTop: '10px' }}>
-			<SearchBar />
-		</div>
-	);
-};
+class App extends React.Component {
+	onSearchSubmit(term) {
+		axios.get('https://api.unsplash.com/search/photos', {
+			params: { query: term },
+			headers: {
+				Authorization:
+					'Client-ID P0Wg9r88rqCMFv1wO1iaKat7QjJyDPwKdLEnvlI6yOY',
+			},
+		});
+	}
+
+	render() {
+		return (
+			<div className='ui container' style={{ marginTop: '10px' }}>
+				<SearchBar onSubmit={this.onSearchSubmit} />
+			</div>
+		);
+	}
+}
 
 export default App;
