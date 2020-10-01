1e2ab4054f15b79cbaa98565d5f6407e4d61869a
dsch <davidschreiterprogramming@gmail.com>
2020-09-30 21:02:53 -0500

    Missing "," in obj, working

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://api.unsplash.com/'
+	baseURL: 'https://api.unsplash.com/',
 	headers: {
 		Authorization: 'Client-ID P0Wg9r88rqCMFv1wO1iaKat7QjJyDPwKdLEnvlI6yOY',
-	}
+	},
 });
-
