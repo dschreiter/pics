c0a328a08ee3b1c959a7c268158691e5de8d967d
dsch <davidschreiterprogramming@gmail.com>
2020-10-02 21:34:24 -0500

    Create Image Card, with supporting CSS

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,19 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
-	const images = props.images.map(({ description, id, urls }) => {
-		return <img alt={description} key={id} src={urls.regular} />;
+	// Option 1
+	// const images = props.images.map(({ description, id, urls }) => {
+	// 	return <img alt={description} key={id} src={urls.regular} />;
+	// });
+
+	//Option 2
+	const images = props.images.map((image) => {
+		return <ImageCard key={image.id} image={image} />;
 	});
 
-	return <div>{images}</div>;
+	return <div className='image-list'>{images}</div>;
 };
 
 export default ImageList;
